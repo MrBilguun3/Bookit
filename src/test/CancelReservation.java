89f4f6b2ce5e66a7e6e668d27a2a386809958724
MrBilguun3 <40651006+MrBilguun3@users.noreply.github.com>
2020-02-29 15:48:11 -0500

    Update CancelReservation.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -3,5 +3,8 @@ package test;
 public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("Arman is pushing codes");
+        
+        System.out.println("Good bye git"); //this is from GitHub
+        
     }
 }
