d507cf2da077f39ddef3012c86890a8b5cc28240
MrBilguun3 <40651006+MrBilguun3@users.noreply.github.com>
2020-02-29 15:54:10 -0500

    Update LocatingMap.java

--- src/test/LocatingMap.java
+++ src/test/LocatingMap.java
@@ -3,5 +3,7 @@ package test;
 public class LocatingMap {
     public static void main(String[] args) {
         System.out.println("aaaaaaa");
+        
+        System.out.println("Hi, how are you? Greetings from line 7 in GitHub!");
     }
 }
