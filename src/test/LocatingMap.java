c7912d2f400ef266df4468ba41ef4c901b30df3f
MrBilguun3 <bilguun0345@gmail.com>
2020-02-29 15:57:30 -0500

    Update LocatingMap.java

--- src/test/LocatingMap.java
+++ src/test/LocatingMap.java
@@ -3,5 +3,7 @@ package test;
 public class LocatingMap {
     public static void main(String[] args) {
         System.out.println("aaaaaaa");
+
+        System.out.println("Hello from line 7 in IntelliJ");
     }
 }
